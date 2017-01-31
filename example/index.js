927f912c31cb119e060f91798478eb885af079e8
Ionică Bizău <bizauionica@gmail.com>
2017-01-31 12:30:42 +0200

    Initial

--- example/index.js
+++ example/index.js
@@ -2,4 +2,39 @@
 
 const uniqueRandomRange = require("../lib");
 
-console.log(uniqueRandomRange());
+
+let rand = uniqueRandomRange(7, 10);
+
+console.log(rand());
+// => 9
+
+console.log(rand());
+// => 7
+
+console.log(rand());
+// => 10
+
+console.log(rand());
+// => 8
+
+console.log(rand());
+// => 9
+
+
+rand = uniqueRandomRange(7, 10);
+console.log(rand());
+// => 7
+
+console.log(rand());
+// => 9
+
+rand.reset();
+
+console.log(rand());
+// => 9
+
+console.log(rand());
+// => 10
+
+console.log(rand());
+// => 7
